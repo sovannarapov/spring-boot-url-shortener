1c618f44a4e73cf0bb0ba38672d5e02e9ee8759f
sovannarapov <sovannarapov9@gmail.com>
2025-03-30 13:57:01 +0700

    feat: add UrlNotFoundException.java

---
+++ src/main/java/com/sovannara/urlshortener/exceptions/UrlNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sovannara.urlshortener.exceptions;
+
+public class UrlNotFoundException extends RuntimeException {
+
+    public UrlNotFoundException(String message) {
+        super(message);
+    }
+
+}
