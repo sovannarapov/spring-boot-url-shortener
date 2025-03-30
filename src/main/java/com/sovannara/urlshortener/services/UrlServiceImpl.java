2490e26d3e3332a1f9fc197dcbe633d2b1ce8728
sovannarapov <sovannarapov9@gmail.com>
2025-03-30 14:09:57 +0700

    feat: add UrlServiceImpl.java

---
+++ src/main/java/com/sovannara/urlshortener/services/UrlServiceImpl.java
@@ -0,0 +1,31 @@
+package com.sovannara.urlshortener.services;
+
+import com.sovannara.urlshortener.entities.ShortenedUrl;
+import com.sovannara.urlshortener.repositories.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UrlServiceImpl implements UrlService {
+
+    private final UrlRepository _repository;
+
+    @Override
+    public boolean existsByCode(String code) {
+        return _repository.existsByCode(code);
+    }
+
+    @Override
+    public Optional<ShortenedUrl> findByCode(String code) {
+        return _repository.findByCode(code);
+    }
+
+    @Override
+    public ShortenedUrl save(ShortenedUrl shortenedUrl) {
+        return _repository.save(shortenedUrl);
+    }
+
+}
