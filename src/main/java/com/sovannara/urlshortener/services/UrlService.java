3591cd6060d5412e67f96215ef62d6b9c16630b4
sovannarapov <sovannarapov9@gmail.com>
2025-03-30 14:01:04 +0700

    feat: add UrlService.java

---
+++ src/main/java/com/sovannara/urlshortener/services/UrlService.java
@@ -0,0 +1,14 @@
+package com.sovannara.urlshortener.services;
+
+import com.sovannara.urlshortener.entities.ShortenedUrl;
+import java.util.Optional;
+
+public interface UrlService {
+
+    boolean existsByCode(String code);
+
+    Optional<ShortenedUrl> findByCode(String code);
+
+    ShortenedUrl save(ShortenedUrl shortenedUrl);
+
+}
